2e9ebca388a6824de521847f8a02ab2e2d532bbe
Bithi-84 <tehzeebbithi.2016@gmail.com>
2024-12-30 00:07:59 +0600

    forntend clear

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -11,10 +11,73 @@ class FrontendController extends Controller
 
 }
 
+public function productDetails(){
+    return view ('products-details');
+
+}
+
 public function shop(){
 
     return view('shop');
+
 }
+
+public function returnProcess(){
+
+    return view('return-process');
+}
+
+public function categoryProducts(){
+
+    return view('category-products');
+}
+
+public function subcategoryProducts(){
+
+    return view('subcategory-products');
+}
+
+public function viewCart(){
+
+    return view('view-cart');
+}
+
+public function checkOut(){
+
+    return view('checkout');
+}
+
+public function privacyPolicy(){
+
+    return view('privacy-policy');
+}
+
+public function termsCondition(){
+
+    return view('terms-conditions');
+}
+
+public function contactUs(){
+
+    return view('contact-us');
+
+}
+
+public function aboutUs(){
+
+    return view('about-us');
+}
+
+public function refundPolicy(){
+
+    return view('refund-policy');
+}
+
+public function paymentPolicy(){
+
+    return view('payment-policy');
+}
+
 
 }
    
