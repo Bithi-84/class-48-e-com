43ffd8cec2b545e0a3656a35ba0543af87643b39
Bithi-84 <tehzeebbithi.2016@gmail.com>
2025-01-10 02:14:59 +0600

    Admin template mastering is in progress

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\support\Facades\Auth;
 class AdminController extends Controller
 {
     public function adminLogin()
     {
         return view('auth.login');
+
     }
-}
+
+  public function adminLogout()
+
+   {    Auth::logout();
+         return redirect('admin/login');
+   }
+   }
