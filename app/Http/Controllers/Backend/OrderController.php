267ffad72e958736f9368e33e649bcfdf4e2ad28
Bithi-84 <tehzeebbithi.2016@gmail.com>
2025-05-21 23:05:15 +0600

    Courier intigretion is done

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class OrderController extends Controller
 {
@@ -37,7 +38,62 @@ class OrderController extends Controller
     public function updateOrderStatus($status,$id)
     {
         $order =Order::find($id);
+        // dd($order);
         $order->status = $status;
+
+        if ($status == "delivered"){
+            if($order -> courier_name == "steadfast"){
+                
+                // Api Endpoint....
+                $endPoint = "https://portal.packzy.com/api/v1/create_order";
+               
+            //    Auth parametres.....
+                $apiKey = "vvxfztmffsbeczeqihxtruibr2dexrlb";
+                $secretKey = "5pqpnmqvbsktdwhwj5t73rkl";
+                $contentType = "application/json";
+
+            // Body parametres...
+
+
+            $invoiceId = $order->invoiceId;
+            $customerName = $order->c_name;
+            $customerPhone = $order->c_phone;
+            $customerAddress = $order->address;
+            $orderPrice = $order->price;
+
+
+
+            // The Header....
+
+            $header = [
+                    
+                'Api-Key' => $apiKey,
+                'Secret-Key' => $secretKey,
+                'Content-Type' => $contentType,
+               
+            ];
+
+            // The Payload....
+            $payload =
+            [
+                 'invoice' => $invoiceId,
+                 'recipient_name'=> $customerName,
+                 'recipient_phone' => $customerPhone,
+                 'recipient_address' => $customerAddress,
+                 'cod_amount' => $orderPrice,
+            ];
+
+           $response = Http::withHeaders($header)->post( $endPoint,  $payload);
+            // dd($response);
+
+            $responseData = $response->json();
+            // dd($responseData);
+             }
+
+            else{
+                return "Select Courier";
+            }
+        }
 
         $order->save();
         return redirect()->back();
